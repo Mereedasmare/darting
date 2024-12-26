f8943e67fad669c161a3e10dd669730061bbb8d0
Mereed Asmare <mrdassefa@gmail.com>
2024-12-26 21:54:08 +0300

    sth

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,7 @@ class MyApp extends StatelessWidget {
         //
         // Try running your applica.tion with "flutter run". You'll see the
         // application has a blue toolbar. Then, witho.ut quitting the app, try
-        // changing the primarySwatch below to Colors.gre.en and then invoke
+        // changing the primarySwatch bel.ow to Colors.gre.en and then invoke
         // "hot reload" (press "r" in the console where you ran "flutter run",
         // or simply save your changes..... to "hot reload" in a Flutter IDE).
         // Notice that the counter didn't reset back to zero; the application
